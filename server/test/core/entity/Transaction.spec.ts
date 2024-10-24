f11cf0437721fc534039e41a7d1258d44ace2f44
Eduardo Rocha <rochaeduardo997@protonmail.com>
2024-10-24 16:19:01 -0300

    changing transaction to accept multiple categories test

--- server/test/core/entity/Transaction.spec.ts
+++ server/test/core/entity/Transaction.spec.ts
@@ -2,7 +2,7 @@ import Transaction, { TransactionDirection } from '../../../src/core/entity/Tran
 import Category from '../../../src/core/entity/Category';
 
 let transaction: Transaction;
-const category = new Category('id', 'name', 'description', 'userId');
+const categories = [ new Category('id', 'name', 'description', 'userId') ];
 const input = {
   id:        'id',
   name:      'name',
@@ -11,10 +11,10 @@ const input = {
   when:      new Date('2022-05-04T00:00:00'),
   createdAt: new Date('2022-05-05T00:00:00'),
   updatedAt: new Date('2022-05-06T00:00:00'),
-  category, userId: 'userId'
+  categories, userId: 'userId'
 };
 
-beforeEach(() => transaction = new Transaction(input.id, input.name, input.value, input.direction, input.when, input.createdAt, input.updatedAt, category, input.userId));
+beforeEach(() => transaction = new Transaction(input.id, input.name, input.value, input.direction, input.when, input.createdAt, input.updatedAt, categories, input.userId));
 
 describe('success', () => {
   test('validate transaction instance', () => {
@@ -25,7 +25,7 @@ describe('success', () => {
     expect(transaction.createdAt).toBe(input.createdAt);
     expect(transaction.updatedAt).toBe(input.updatedAt);
     expect(transaction.when).toBe(input.when);
-    expect(transaction.category).toBe(input.category);
+    expect(transaction.categories).toEqual(input.categories);
     expect(transaction.userId).toBe(input.userId);
   });
 
@@ -36,24 +36,24 @@ describe('success', () => {
   });
 
   test('transaction instance with value equal 0', () => {
-    transaction = new Transaction(input.id, input.name, 0, input.direction, input.when, input.createdAt, input.updatedAt, category);
+    transaction = new Transaction(input.id, input.name, 0, input.direction, input.when, input.createdAt, input.updatedAt, categories);
     expect(transaction.value).toBe(0);
   });
 
   test('transaction instance without category', () => {
     transaction = new Transaction(input.id, input.name, 0, input.direction, input.when, input.createdAt, input.updatedAt);
-    expect(transaction.category).toBeUndefined();
+    expect(transaction.categories).toHaveLength(0);
   });
 });
 
 describe('fail', () => {
   test('value less than 0', () => {
-    expect(() => new Transaction(input.id, input.name, -1, input.direction, input.when, input.createdAt, input.updatedAt, category))
+    expect(() => new Transaction(input.id, input.name, -1, input.direction, input.when, input.createdAt, input.updatedAt, categories))
       .toThrow('value must be greater or equal 0');
   });
 
   test('value less than 0', () => {
-    expect(() => new Transaction(input.id, input.name, input.value, input.direction, new Date('2030-01-01'), input.createdAt, input.updatedAt, category))
+    expect(() => new Transaction(input.id, input.name, input.value, input.direction, new Date('2030-01-01'), input.createdAt, input.updatedAt, categories))
       .toThrow('when date must be before now');
   });
 });
